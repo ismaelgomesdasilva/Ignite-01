e6dcf54195d9af02d6dc044e3dece0ea0b484379
Ismael Gomes <ismaelgomesdasilva003@gmail.com>
2023-03-11 16:00:08 -0300

    feat: application structure - Component: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
-import { Header } from "./components/Header";
-import { Post } from "./components/Post"
-import './global.css';
-function App() {
+import { Header } from './components/Header';
+import { Post } from './components/Post'
+import { Sidebar } from './components/Siderbar';
 
+import './global.css';
+import styles from './app.module.css';
+
+export default function App() {
   return (
-    <>
-    <Header/>
-      <Post
-        author='Ismael Gomes'
-        content='Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate ad voluptatem vero quaerat, minima fugit dolores nisi at repudiandae quibusdam hic facere dignissimos quia consectetur quo animi velit obcaecati et!'
-      />
-    </>
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Diego Fernandes"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
+          />
+          <Post
+            author="Gabriel Buzzi"
+            content="Um novo post muito legal"
+          />
+        </main>
+      </div>
+    </div>
   )
 }
-
-export default App
