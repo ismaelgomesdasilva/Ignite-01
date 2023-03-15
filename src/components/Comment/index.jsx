785e06cc92f216672ec87dda280fb014c2caf078
Ismael Gomes <ismaelgomesdasilva003@gmail.com>
2023-03-15 13:13:36 -0300

    refactor: the React Engines - Understanding Closures in React

--- src/components/Comment/index.jsx
+++ src/components/Comment/index.jsx
@@ -12,7 +12,9 @@ export function Comment({ content, onDeleteComment }) {
     onDeleteComment(content);
   }
 function handleLikeComment(){
-  setLikeCount(likeCount + 1)
+  setLikeCount((states)=>{
+    return states + 1
+  })
 }
   return (
     <div className={styles.comment}>
